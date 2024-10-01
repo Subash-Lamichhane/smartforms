635a0db32ea9618804c149cbb1907eec69f34ddc
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-10-01 16:14:02 +0545

    specific question can be updated

--- frontend/src/pages/QuizGenerationPage.jsx
+++ frontend/src/pages/QuizGenerationPage.jsx
@@ -26,8 +26,63 @@ const QuizGenerationPage = () => {
 
 const QuizGenerationPageExtend = () => {
     const [quizzes, setQuizzes] = useState([]);
+    useCopilotReadable({
+        description: "The current set of quiz questions are: ",
+        value: quizzes,
+    });
 
-    console.log(quizzes);
+    // useCopilotAction({
+    //     name: "updateQuestion",
+    //     description: "Change or Update the question using question number provided by user.",
+    //     parameters:[{
+    //         name: "updatedQuiz",
+    //         type: "object",
+    //         description: "The new quizzes",
+    //         attributes: [
+    //             {
+    //                 name: 'question',
+    //                 type: 'string',
+    //                 description: 'The quiz question.'
+    //             },
+    //             {
+    //                 name: "options",
+    //                 type: "string[]",
+    //                 description: "The options for quiz",
+    //             },
+    //             {
+    //                 name: 'hint',
+    //                 type: 'string',
+    //                 description: 'The hint to the question.'
+    //             },
+    //             {
+    //                 name: 'answer',
+    //                 type: 'string',
+    //                 description: 'Correct answer among options.'
+    //             },
+    //         ]},
+    //         {
+    //             name:"indexNo",
+    //             type: "number",
+    //             description: "The index number of question to be updated."
+    //         }  
+    //     ],
+    //     handler: async ({ updatedQuiz, indexNo }) => {
+    //         setQuizzes(prevQuizzes => {
+    //             console.log(indexNo)
+    //             if (indexNo < 0 || indexNo >= prevQuizzes.length) {
+    //                 console.error("Invalid index number");
+    //                 return prevQuizzes;
+    //             }
+    
+    //             // Create a new array with the updated question at the specified index
+    //             const updatedQuizzes = [...prevQuizzes];
+    //             updatedQuizzes[indexNo] = updatedQuiz;
+    
+    //             return updatedQuizzes;
+    //         });
+    //         console.log(quizzes)
+    //     }
+    // })
 
     useCopilotReadable({
         description: "This is the set of quizzes: ",
@@ -35,11 +90,11 @@ const QuizGenerationPageExtend = () => {
     });
 
     useCopilotAction({
-        name: "newQuestions",
+        name: "newQuizzes",
         description: "Provide quizzes in MCQ format based on conditions given. Do not tell questions and options in chat. Generate only the number of questions asked in the chat.",
         parameters: [
             {
-                name: "newQuizzes",
+                name: "newQuiz",
                 type: "object[]",
                 description: "The new quizzes",
                 attributes: [
@@ -66,11 +121,17 @@ const QuizGenerationPageExtend = () => {
                 ]
             },
         ],
-        handler: ({ newQuizzes }) => {
-            setQuizzes(prevQuizzes => [...prevQuizzes, ...newQuizzes]);
-        },
-    });
+        handler: async ({ newQuiz }) => {
+        // Create a new array and append new quizzes to it
+        let updatedQuizzes = []
+        updatedQuizzes = [...updatedQuizzes,...newQuiz];
 
+        // Replace old quizzes with the new quizzes
+        setQuizzes(updatedQuizzes);
+        },
+        render: "Generating your quiz...",
+    });
+    
     return (
         <div>
             <h1>Quiz List</h1>
