7e1f5f3f9d282468c75a18fba880b57d52d1d370
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-10-02 20:53:41 +0545

    Design the ui for creating smartforms using copilotkit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import QuizGenerationPage from "./pages/QuizGenerationPage";
 import LandingPage from "./pages/LandingPage";
 import SmartFormEntry from "./components/SmartFormEntry";
 import NavBar from "./components/NavBar";
 import SmartForm from "./pages/SmartForm";
 import ResponsePage from "./pages/ResponsePage";
+import CreateSmartFormPage from "./pages/CreateSmartFormPage";  
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/create-smartforms" element={<QuizGenerationPage />} />
+        <Route path="/smartforms/create" element={<CreateSmartFormPage />} />
         <Route path="/smartforms/:id" element={<SmartForm />} />
         <Route path="/responses" element={<ResponsePage/>} />
         <Route
---
+++ frontend/src/pages/CreateSmartFormPage.jsx
@@ -0,0 +1,13 @@
+import GenerateSmartForm from "../components/GenerateSmartForm";
+import NavBar from "../components/NavBar";
+
+const CreateSmartFormPage = () => {
+  return (
+    <div>
+      <NavBar />
+      <GenerateSmartForm />
+    </div>
+  );
+};
+
+export default CreateSmartFormPage;
